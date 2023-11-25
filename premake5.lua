1d9f67dca04cdb4f3ed5a3cb3f1cc1cedf7492ff
Youri Schijff <90404399+YouriSchijff@users.noreply.github.com>
2023-11-25 20:54:33 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -19,11 +19,11 @@ project "imgui"
 		"imstb_truetype.h",
 		"imgui_demo.cpp",
 
-    "backend/imgui_impl_glfw.h",
-    "backend/imgui_impl_glfw.cpp",
-    "backend/imgui_impl_opengl3.h",
-    "backend/imgui_impl_opengl3.cpp",
-    "backend/imgui_impl_opengl3_loader.h"
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.h",
+        "backends/imgui_impl_opengl3.cpp",
+        "backends/imgui_impl_opengl3_loader.h"
 	}
 
   filter "system:linux"
