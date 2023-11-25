897ef164b98644b3569a3fdb537c456e540023d8
Youri Schijff <90404399+YouriSchijff@users.noreply.github.com>
2023-11-25 23:17:54 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -17,13 +17,7 @@ project "imgui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp",
-
-        "backends/imgui_impl_glfw.h",
-        "backends/imgui_impl_glfw.cpp",
-        "backends/imgui_impl_opengl3.h",
-        "backends/imgui_impl_opengl3.cpp",
-        "backends/imgui_impl_opengl3_loader.h"
+		"imgui_demo.cpp"
 	}
 
     includedirs {
